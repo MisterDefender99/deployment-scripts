ffaf665031bb29b11a6b4a1f9eb441b833d9a359
bhawnachauhan07 <92302182+bhawnachauhan07@users.noreply.github.com>
2022-10-31 14:46:32 +0530

    i have updated the code

--- 01_deploy_CoinA.js
+++ 01_deploy_CoinA.js
@@ -11,6 +11,12 @@ module.exports = async ({
        gasLimit: 4000000,
        args: [],
      });
+     const {deployer2, deployer1, deployer3} = await getNamedAccounts();
+     function approveAll(address spender, uint tokens) external override returns (bool){
+        approved[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
      console.log("CoinA contract is deployed at: ",contractAdeployed.address);
      console.log("Deployer of contract CoinA is: ",deployer1);
      };
